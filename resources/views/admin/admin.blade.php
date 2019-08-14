573cc6efcfc0e9200c9f5019d02c9a206d154c7e
Your Name <you@example.com>
2019-08-14 13:39:38 +0800

    update

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -129,6 +129,7 @@
                                         {{ __('Logout') }}
                                     </a>
 
+                                    
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
