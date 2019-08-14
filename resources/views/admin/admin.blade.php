bd9d03f37d349eaed064c13abf93b388ad3428ff
Your Name <you@example.com>
2019-08-14 13:31:04 +0800

    update

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -117,15 +117,23 @@
 							<h2>ADMIN DASHBOARD</h2>
 						</div>
 						<div class="col-lg-6">
-							<div style="float: right;" class="dropdown">
-							  <button style="color: #000; background-color: #fff; border: none;" class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-							    Admin
-							  </button>
-							  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-							    <a class="dropdown-item" href="#">Logout</a>
-							   
-							  </div>
-							</div>
+							<li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
 						</div>
 					</div>
 				</div>
