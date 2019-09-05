b4e5e891455c5ad3790c50a359a22fe7590128aa
James-Russel_Bonos <james.russel0603@gmail.com>
2019-09-05 14:48:23 +0800

    update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,7 +29,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/employee/employee';
+    protected $redirectTo = '/employee';
 
     /**
      * Create a new controller instance.
