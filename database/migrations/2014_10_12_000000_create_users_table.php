a11031f928502412d77f686744fd47355797cd22
James-Russel_Bonos <james.russel0603@gmail.com>
2019-09-09 14:54:45 +0800

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,6 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
             $table->string('employee_id')->unique();
             $table->string('email')->unique();
             $table->integer('role')->default('2');
