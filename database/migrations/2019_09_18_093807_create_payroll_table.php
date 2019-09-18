11b7af2e3570cfee5df8d0f4646afaa8350178eb
James-Russel_Bonos <james.russel0603@gmail.com>
2019-09-18 10:32:09 +0800

    update

---
+++ database/migrations/2019_09_18_093807_create_payroll_table.php
@@ -0,0 +1,68 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePayrollTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payrolls', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('employee_id');
+            $table->double('no_days_worked', 8, 2);
+            $table->double('daily_rate', 8, 2);
+            $table->double('rate_per_hour', 8, 2);
+            $table->double('basic_pay', 8, 2);
+            $table->double('total_absences', 8, 2);
+            $table->double('unpaid_absences', 8, 2);
+            $table->double('charged_to_SIL', 8, 2);
+            $table->double('amount_absences', 8, 2);
+            $table->double('allowance', 8, 2);
+            $table->double('adjustment_incentives', 8, 2);
+            $table->double('no_legal_holidays', 8, 2);
+            $table->double('amount_holidays', 8, 2);
+            $table->double('regular_OT_hours', 8, 2);
+            $table->double('regular_OT_amount', 8, 2);
+            $table->double('restday_OT_hours', 8, 2);
+            $table->double('restday_OT_amount', 8, 2);
+            $table->double('legal_holiday_OT_hours', 8, 2);
+            $table->double('legal_holiday_OT_amount', 8, 2);
+            $table->double('special_holiday_OT_hours', 8, 2);
+            $table->double('special_holiday_OT_amount', 8, 2);
+            $table->double('no_hours_rendered', 8, 2);
+            $table->double('amount_night_diffential', 8, 2);
+            $table->double('no_hours_undertime', 8, 2);
+            $table->double('no_hours_late', 8, 2);
+            $table->double('gross_pay', 8, 2);
+            $table->double('SSS_payroll', 8, 2);
+            $table->double('PHIC_payroll', 8, 2);
+            $table->double('HDMF_payroll', 8, 2);
+            $table->double('total_deduction_benefits', 8, 2);
+            $table->double('SSS_loan', 8, 2);
+            $table->double('company_loan', 8, 2);
+            $table->double('HDMF_loan', 8, 2);
+            $table->double('uniform', 8, 2);
+            $table->double('total_deduction_loan', 8, 2);
+            $table->double('tax', 8, 2);
+            $table->double('net_pay', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payroll');
+    }
+}
