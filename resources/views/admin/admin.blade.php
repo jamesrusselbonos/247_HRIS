ea5f05261c233057f13654490864038506ffe1ad
James-Russel_Bonos <james.russel0603@gmail.com>
2019-09-23 14:22:44 +0800

    update

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -196,7 +196,7 @@
 		<div class="col-lg-10 main">
 			
 			
-				<div class="dashboard_banner">
+				<!-- <div class="dashboard_banner">
 					<div class="row">
 						<div class="col-lg-6">
 							<h6><a href="/admin" style="color: #000;">ADMIN DASHBOARD</a></h6>
@@ -210,7 +210,67 @@
 							</div>
 						</div>
 					</div>
-				</div>
+				</div> -->
+
+				<nav class="navbar navbar-expand-lg navbar-light bg-light">
+					<button style="border: none; background-color: transparent; height: 40px; width: 40px; margin-right: 30px;"><i style="font-size: 20px;" class="fa fa-bars" aria-hidden="true"></i></button>
+
+				  	<a class="navbar-brand" href="/admin" >Admin Dashboard</a>
+
+				  	<ul class="nav navbar-nav ml-auto">
+				      	<li class="nav-item dropdown">
+                            <a style="color: #000;" id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <i style="font-size: 10px;" class="fa fa-chevron-down" aria-hidden="true"></i>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+				    </ul>
+
+				  	<!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				    	<span class="navbar-toggler-icon"></span>
+				  	</button> -->
+
+				  	<!-- <div class="collapse navbar-collapse" id="navbarSupportedContent">
+				    	<ul class="navbar-nav mr-auto">
+				      		<li class="nav-item active">
+				        		<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+				      		</li>
+				      		<li class="nav-item">
+				        		<a class="nav-link" href="#">Link</a>
+				      		</li>
+				      		<li class="nav-item dropdown">
+				        		<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				          		Dropdown
+				        		</a>
+				        		<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+				          			<a class="dropdown-item" href="#">Action</a>
+				          			<a class="dropdown-item" href="#">Another action</a>
+				          			<div class="dropdown-divider"></div>
+				          			<a class="dropdown-item" href="#">Something else here</a>
+				        		</div>
+				      		</li>
+				      		<li class="nav-item">
+				        		<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+				      		</li>
+				    	</ul>
+				    	<form class="form-inline my-2 my-lg-0">
+				     		<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+				      		<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+				    	</form>
+				  	</div> -->
+				</nav>
+
 				@yield('content')
 			
 		</div>
--- resources/views/employee/employee.blade.php
+++ resources/views/employee/employee.blade.php
@@ -117,7 +117,7 @@
 			</div>
 		</div>
 		<div class="col-lg-10 main">
-			<div class="dashboard_banner">
+			<!-- <div class="dashboard_banner">
 				<div class="row">
 					<div class="col-lg-6">
 						<h6>EMPLOYEE DASHBOARD</h6>
@@ -138,7 +138,7 @@
 	                                     <span><img style="height: 40px; width: 40px; border-radius: 100%;" src="{{ Auth::user()->employee()->first()->employee_img }}"> &nbsp;&nbsp;&nbsp;{{ Auth::user()->employee()->first()->firstname }}</span>
 	                                </a>
 
-	                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+	                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
 	                                	<a class="dropdown-item" href="#">
 	                                        <i class="fa fa-user" aria-hidden="true"></i> &nbsp;Profile
 	                                    </a>
@@ -157,7 +157,73 @@
 						</ul>
 					</div>
 				</div>
-			</div>
+			</div> -->
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				<button style="border: none; background-color: transparent; height: 40px; width: 40px; margin-right: 30px;"><i style="font-size: 20px;" class="fa fa-bars" aria-hidden="true"></i></button>
+
+			  	<a class="navbar-brand" href="/employee" >Employee Dashboard</a>
+
+			  	<ul class="nav navbar-nav ml-auto">
+			  		<li class="nav-item">
+			  			<a href="#" class="notification-trigger">
+							  <span><i style="font-size: 20px;" class="fa fa-bell-o" aria-hidden="true"></i></span>
+							  @if(auth()->user()->unreadNotifications->count())
+							  <span class="badge" id="bell_badge">{{auth()->user()->unreadNotifications->count()}}</span>
+							  @endif
+						</a>
+			  		</li>
+			      	<li class="nav-item dropdown" style="padding-top: 10px;">
+                        <a style="color: #000;" id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                             <span><img style="height: 35px; width: 35px; border-radius: 100%;" src="{{ Auth::user()->employee()->first()->employee_img }}"> &nbsp;&nbsp;&nbsp;{{ Auth::user()->employee()->first()->firstname }} <i style="font-size: 10px;" class="fa fa-chevron-down" aria-hidden="true"></i></span>
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+			    </ul>
+
+			  	<!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			    	<span class="navbar-toggler-icon"></span>
+			  	</button> -->
+
+			  	<!-- <div class="collapse navbar-collapse" id="navbarSupportedContent">
+			    	<ul class="navbar-nav mr-auto">
+			      		<li class="nav-item active">
+			        		<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+			      		</li>
+			      		<li class="nav-item">
+			        		<a class="nav-link" href="#">Link</a>
+			      		</li>
+			      		<li class="nav-item dropdown">
+			        		<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			          		Dropdown
+			        		</a>
+			        		<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+			          			<a class="dropdown-item" href="#">Action</a>
+			          			<a class="dropdown-item" href="#">Another action</a>
+			          			<div class="dropdown-divider"></div>
+			          			<a class="dropdown-item" href="#">Something else here</a>
+			        		</div>
+			      		</li>
+			      		<li class="nav-item">
+			        		<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+			      		</li>
+			    	</ul>
+			    	<form class="form-inline my-2 my-lg-0">
+			     		<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+			      		<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+			    	</form>
+			  	</div> -->
+			</nav>
 			<div class="panel">
 			     <div class="title">Notifications</div>
 				      <ul class="notification-bar" id="not">
