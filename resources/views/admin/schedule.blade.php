93da75496d564d447b1afbedbc810d09b8cc0253
James-Russel_Bonos <james.russel0603@gmail.com>
2019-09-30 16:29:14 +0800

    update

--- resources/views/admin/schedule.blade.php
+++ resources/views/admin/schedule.blade.php
@@ -193,13 +193,13 @@
 </div>
 
 <div class="modal fade bd-example-modal-lg" id="mod_sched" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-	  <div class="modal-dialog modal-lg">
+	<div class="modal-dialog modal-lg">
 	    <div class="modal-content">
-	      <div class="modal-header">
-	          <h5 class="modal-title" id="exampleModalLabel">Add A Schedule</h5>
-	          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-	            <span aria-hidden="true">&times;</span>
-	          </button>
+	      	<div class="modal-header">
+	          	<h5 class="modal-title" id="exampleModalLabel">Add A Schedule</h5>
+	          	<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	            	<span aria-hidden="true">&times;</span>
+	          	</button>
 	       </div>
 	       <div class="modal-body">
 	              <form enctype="multipart/form-data">
